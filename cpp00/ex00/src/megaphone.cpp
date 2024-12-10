b894b4c2103d51a13d8c9a8ea3fac55991e504c8
ahenault <ahenault@student.42.fr>
2024-12-10 19:01:13 +0100

    changed ex00

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -6,8 +6,9 @@ int main(int argc, char **argv)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     for (int i(1); argv[i]; i++)
     {
-        for (int j (0); argv[i][j]; j++)
-			std::cout << (char) toupper(argv[i][j]);
+		std::string str = argv[i];
+        for (size_t j(0); j < str.length(); j++)
+			std::cout << (char) toupper(str[j]);
     }
     std::cout << std:: endl;
     return (0);
