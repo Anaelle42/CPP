2b03a1ef083e21e45ddc2bc93637b70e4934b66b
ahenault <ahenault@student.42.fr>
2024-12-13 22:52:01 +0100

    continued ex03

--- cpp03/ex03/srcs/DiamondTrap.cpp
+++ cpp03/ex03/srcs/DiamondTrap.cpp
@@ -1,11 +1,19 @@
 #include "../includes/DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name", 10, 10, 1), ScavTrap("scav"), FragTrap("frag")
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), _name(name)
 {
-	
-	this->_name = name;
-	std::cout << ScavTrap::getAttack() << std::endl;
+	// frag est initialise apres scav, ca remplace ses valeurs
+	std::cout << "energie de scav: " << ScavTrap::_energyPoints << std::endl; //
+
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
 	std::cout << "Default constructor Diamond Trap called" << std::endl;
+
+	std::cout << "Name : " << this->_name << std::endl;
+	std::cout << "Hit points: " << this->_hitPoints << std::endl;
+	std::cout << "Energy Points : " << this->_hitPoints << std::endl; // prends valeur de frag
+	std::cout << "Attack damage : " << this->_attackDamage << std::endl;
 }
 
 DiamondTrap::~DiamondTrap()
--- cpp03/ex03/srcs/main.cpp
+++ cpp03/ex03/srcs/main.cpp
@@ -2,15 +2,9 @@
 
 int main()
 {
-	ClapTrap clap;
-	FragTrap frag;
-	ScavTrap scav;
-	std::cout << "attack : " << clap.getAttack() << std::endl;
-	std::cout << "attack : " << frag.getAttack() << std::endl;
-	std::cout << "attack : " << scav.getAttack() << std::endl;
-
 	DiamondTrap max("Max");
-	// std::cout << "attack : " << max.getAttack() << std::endl;
+	max.whoAmI();
+	max.attack("bob"); // mauvais nom
 
 	return 0;
 }
