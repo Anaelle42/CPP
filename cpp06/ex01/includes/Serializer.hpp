7f481beaa783cd67c25ceea88aa85aaa18647135
ahenault <ahenault@student.42.fr>
2025-03-31 23:47:39 +0200

    push ex01

---
+++ cpp06/ex01/includes/Serializer.hpp
@@ -0,0 +1,25 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include <stdint.h>
+
+typedef struct s_data
+{
+    std::string type;
+    int         nb;
+}					Data;
+
+class Serializer
+{
+    public:
+        Serializer();
+        Serializer(const Serializer& other);
+        Serializer& operator=(const Serializer& other);
+        ~Serializer();
+
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+};
+
+#endif
---
+++ cpp06/ex01/srcs/main.cpp
@@ -0,0 +1,15 @@
+#include "../includes/Serializer.hpp"
+
+int main()
+{
+	Data data;
+	data.nb = 10;
+	data.type = "int";
+
+	std::cout << &data << ", " << data.type << ": " << data.nb << std::endl;
+
+	Data *ptr = Serializer::deserialize(Serializer::serialize(&data));
+	std::cout << ptr << ", " << ptr->type << ": " << ptr->nb << std::endl;
+
+	return (0);
+}
