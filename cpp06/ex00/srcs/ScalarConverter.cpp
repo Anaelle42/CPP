40f9ad70b689f487098b92271a315ee69d365ab8
ahenault <ahenault@student.42.fr>
2025-03-31 22:05:40 +0200

    ex00

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -53,7 +53,7 @@ void toDouble(std::string string)
     
     int i = static_cast<int>(d);
     char c = static_cast<char>(d);
-    float f = static_cast<double>(d);
+    float f = static_cast<float>(d);
 
     std::cout << "char: " << c << std::endl;
     std::cout << "int: " << i << std::endl;
@@ -63,11 +63,12 @@ void toDouble(std::string string)
 
 void toFloat(std::string string)
 {
+    std::cout << "float" << std::endl;
     float f = strtof(string.c_str(), NULL);
     
     int i = static_cast<int>(f);
     char c = static_cast<char>(f);
-    double d = static_cast<float>(f);
+    double d = static_cast<double>(f);
 
     std::cout << "char: " << c << std::endl;
     std::cout << "int: " << i << std::endl;
@@ -75,7 +76,71 @@ void toFloat(std::string string)
     std::cout << "double: " << d << std::endl;
 }
 
+int    printNan()
+{
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: nanf" << std::endl;
+    std::cout << "double: nan" << std::endl;
+    return (1);
+}
+
+int    printInf(char sign)
+{
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: " << sign << "inff" << std::endl;
+    std::cout << "double: " << sign << "inf" << std::endl;
+    return (1);
+}
+
+int isSpecial(std::string s)
+{
+    if (s.compare("nan") == 0 || s.compare("nanf") == 0)
+        return (printNan());
+    if (s.compare("+inf") == 0 || s.compare("+inff") == 0)
+        return (printInf('+'));
+    if (s.compare("-inf") == 0 || s.compare("-inff") == 0)
+        return (printInf('-'));
+    return (0);
+}
+
+void checkString(std::string s)
+{
+    if (s.empty())
+        return ;
+    if (isSpecial(s))
+        return;
+
+    if (s.length() == 1)
+        return (toChar(s));
+    
+    int point = 0;
+
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '.')
+            point++;
+        else if ((i == 0 && (s[i] == '+' || s[i] == '-')) || (i == (s.length() -1) && s[i] == 'f'))
+            i++;
+        else if (s[i] != '\0' && !std::isdigit(s[i])) 
+        {
+            std::cout << "invalid" << std::endl;
+            return ;
+        }
+    }
+    if (point > 1)
+        return ;
+
+    if (point == 0)
+        return (toInt(s));
+    if (s[s.length() - 1] == 'f')
+        return (toFloat(s));
+    else
+        toDouble(s);
+}
+
 void ScalarConverter::convert(std::string string)
 {
-    toFloat(string);
+    checkString(string);
 }
