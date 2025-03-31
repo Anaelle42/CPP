eba4b0cb45ea3b956fc4c591e15d2accdb6c0f7e
ahenault <ahenault@student.42.fr>
2025-04-01 00:36:51 +0200

    push ex02

---
+++ cpp06/ex02/srcs/main.cpp
@@ -0,0 +1,74 @@
+#include "../includes/A.hpp"
+#include "../includes/B.hpp"
+#include "../includes/C.hpp"
+
+Base * generate(void)
+{
+	int randomNum = rand() % 3;
+	if (!randomNum)
+		return (new A);
+	if (randomNum == 1)
+		return (new B);
+	else
+		return (new C);
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+
+	if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+
+	if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception &e)
+	{}
+
+	try
+	{
+		dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception &e)
+	{}
+
+	try
+	{
+		dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception &e)
+	{}
+}
+
+int main()
+{
+	srand(time(0));
+
+	Base *base = generate();
+	identify(base);
+	identify(*base);
+
+	std::cout << std::endl;
+	Base *base2 = generate();
+	identify(base2);
+	identify(*base2);
+
+	std::cout << std::endl;
+	Base *base3 = generate();
+	identify(base3);
+	identify(*base3);
+
+	return (0);
+}
