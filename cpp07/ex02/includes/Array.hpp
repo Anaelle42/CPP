fbdca6a986782b5081012e8be83cf68170a3e118
ahenault <ahenault@student.42.fr>
2025-04-14 18:04:03 +0200

    push ex02

--- cpp07/ex02/includes/Array.hpp
+++ cpp07/ex02/includes/Array.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 
 template <typename T>
-
 class Array
 {
 	private:
@@ -37,19 +36,16 @@ class Array
 				delete[] this->_array;
 				this->_array = new T[this->_size];
 				for (int i = 0; i < this->_size; i++)
-				{
 					this->_array[i] = other._array[i];
-				}
 			}
 			return (this);
 		}
 
 		T& operator[](int index)
 		{
-			if (index >= 0 && index < size())
-				return (this->_array[index]);
-			else
+			if (index < 0 || index >= size())
 				throw Array::BadIndexException();
+			return (this->_array[index]);
 		}
 
 		~Array()
--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,22 +6,26 @@ int main()
 	{
 		Array <int> tabA;
 		Array <int> tabB(5);
-		Array <int> tabC(tabA);
+		for (int i = 0; i < tabB.size(); i++)
+			tabB[i] = i;
+		Array <int> tabC(tabB);
+
 		std::cout << "size A: " << tabA.size() << std::endl;
 		std::cout << "size B: " << tabB.size() << std::endl;
 		std::cout << "size C: " << tabC.size() << std::endl;
-
-		for (int i = 0; i < tabB.size(); i++)
-			tabB[i] = i;
-		for (int i = 0; i < tabB.size() + 5; i++)
-			std::cout << tabB[i] << " ";
+		
+		for (int i = 0; i < tabC.size() + 5; i++)
+			std::cout << tabC[i] << " ";
 
 		Array <std::string> tabD;
 		Array <std::string> tabE(4);
+
 		for (int i = 0; i < tabE.size(); i++)
 			tabE[i] = "hello";
-		for (int i = 0; i < tabE.size() + 5; i++)
+		for (int i = 0; i < tabE.size(); i++)
 			std::cout << tabE[i] << " ";
+		
+		std::cout << std::endl << tabE[5] << std::endl;
 
 		int* a = new int();
 		std::cout << *a << std::endl;
